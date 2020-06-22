84814c4b5d355df235d5e65b6d267f925337555d
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-22 19:11:09 -0400

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,28 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  organize_collection_by_symbol(:item,collection,name)[0]
+
 
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-end
-
-
+    
+  cart.map do |element|
+    element[:count] = organize_collection_by_symbol(:item,cart,element[:item]).count {|i| i[:item] == element[:item]}
+    element
   
+    
+  
+  
+  end
+  
+  end
+
+def organize_collection_by_symbol(symbol,collection,search_value)
+  collection.filter {|i| i[symbol]==search_value}
+end  
+
